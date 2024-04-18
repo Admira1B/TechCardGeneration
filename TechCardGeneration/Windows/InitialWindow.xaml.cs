037def73e45abd73a4c8006598fff20d867e6503
Admira1B <vreizenbuk@mail.ru>
2024-04-18 17:22:17 +0700

    UI uppdated to onewindow

--- TechCardGeneration/Windows/InitialWindow.xaml.cs
+++ TechCardGeneration/Windows/InitialWindow.xaml.cs
@@ -6,12 +6,15 @@ namespace TechCardGeneration.Windows
 {
     public partial class InitialWindow : Window
     {
+        private bool isContinueButtonClicked = false;
+
         public InitialWindow()
         {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
 
+        // События клика кнопок
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
             ShowCreatingElements();
@@ -34,11 +37,35 @@ namespace TechCardGeneration.Windows
                 return;
             }
 
-            GeneratingWindow generatingWindow = new GeneratingWindow(studentsNumber, columnsNumber);
-            generatingWindow.Show();
-            this.Close();
+            TextBoxGenerator.Generation(studentsNumber, StudentTextBoxContainer, 430);
+            TextBoxGenerator.Generation(columnsNumber, ColumnNameTextBoxContainer, 300);
+            TextBoxGenerator.Generation(columnsNumber, ColumnСoefficientTextBoxContainer, 300);
+
+            ShowStudentInfo();
         }
 
+        private void ContinueGeneratingButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isContinueButtonClicked)
+            {
+                ShowFileCreatingElements();
+                return;
+            }
+
+            isContinueButtonClicked = true;
+            ShowColumnInfo();
+        }
+
+        private void FinishButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(PathInputTextBox.Text) || string.IsNullOrWhiteSpace(FileNameInputTextBox.Text))
+            {
+                MessageBox.Show("Внимательно проверьте, чтобы все поля были заполнены.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        // Проверка на ввод чисел
         private void StudentsInputTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
         {
             NumberInputCheck(e);
@@ -49,6 +76,16 @@ namespace TechCardGeneration.Windows
             NumberInputCheck(e);
         }
 
+        // Метод для проверки на ввод числа
+        private void NumberInputCheck(System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Показ элементов интерфейса
         private void ShowCreatingElements()
         {
             InitialWindowMainSP.Visibility = Visibility.Collapsed;
@@ -59,12 +96,39 @@ namespace TechCardGeneration.Windows
             CreatingWindowControlSP.Visibility = Visibility.Visible;
         }
 
-        private void NumberInputCheck(System.Windows.Input.TextCompositionEventArgs e) 
+        private void ShowStudentInfo()
         {
-            if (!char.IsDigit(e.Text, 0))
-            {
-                e.Handled = true;
-            }
+            CreatingLabelsWindowMainSP.Visibility = Visibility.Collapsed;
+            CreatingTextBoxesWindowMainSP.Visibility = Visibility.Collapsed;
+            CreatingWindowControlSP.Visibility = Visibility.Collapsed;
+
+            StudentLabelSP.Visibility = Visibility.Visible;
+            StudentSP.Visibility = Visibility.Visible;
+            GeneratingWindowControlSP.Visibility = Visibility.Visible;
+        }
+
+        private void ShowColumnInfo()
+        {
+            StudentLabelSP.Visibility = Visibility.Collapsed;
+            StudentSP.Visibility = Visibility.Collapsed;
+
+            ColumnNameLabelSP.Visibility = Visibility.Visible;
+            ColumnNameSP.Visibility = Visibility.Visible;
+            ColumnCoefficientLabelSP.Visibility = Visibility.Visible;
+            ColumnCoefficientSP.Visibility = Visibility.Visible;
+        }
+
+        private void ShowFileCreatingElements()
+        {
+            ColumnNameLabelSP.Visibility = Visibility.Collapsed;
+            ColumnNameSP.Visibility = Visibility.Collapsed;
+            ColumnCoefficientLabelSP.Visibility = Visibility.Collapsed;
+            ColumnCoefficientSP.Visibility = Visibility.Collapsed;
+            GeneratingWindowControlSP.Visibility = Visibility.Collapsed;
+
+            CreatingLabelsWindowSP.Visibility = Visibility.Visible;
+            CreatingTextBoxesWindowSP.Visibility = Visibility.Visible;
+            FinishingWindowControlSP.Visibility = Visibility.Visible;
         }
     }
 }
